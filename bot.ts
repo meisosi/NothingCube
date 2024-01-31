b0099b00f50059e2eea685b1b82db642f365558a
Osirias-ru <busorgin.art2007@gmail.com>
2024-01-31 23:48:39 +0400

    Фикс конфиликтов

--- bot.ts
+++ bot.ts
@@ -1,7 +1,6 @@
 import { Telegraf } from "telegraf";
 import { TextExampleModule } from "./src/examples/modules/textExampleModule";
 import { KeyboardExampleModule } from "./src/examples/modules/keyboardExampleModule";
-import { ErrorModule } from "./src/errors/modules/errorModule";
 
 export class Bot {
     constructor(private readonly telegraf: Telegraf) {
@@ -11,8 +10,6 @@ export class Bot {
     private launch() {
         new TextExampleModule().init(this.telegraf);
         new KeyboardExampleModule().init(this.telegraf);
-        new ErrorModule().init(this.telegraf);
-
         this.telegraf.launch();
     }
 }
